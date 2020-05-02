151c6977fdb03c7f071da8743cdb3e9a303f19aa
Parker Davis <parkersamueldavis@gmail.com>
2020-05-02 11:32:11 -0700

    Drop Down Added for Commands

--- src/App.js
+++ src/App.js
@@ -21,26 +21,29 @@ import FormControl from '@material-ui/core/FormControl';
 
 
 var startByte = "CC"; 
-
 var addProjectCMD = "10";
 var addChipCMD = "20";
 var addFileCMD = "30";
+var addAllCMD = "00";
 var delProjectCMD = "40";
 var delChipCMD = "50";
 var delfileCMD = "60"; 
 
-
-
 class App extends Component {
-
+  constructor(props) {
+    super(props)
+    this.selectRef = React.createRef()
+  }
+  
   state = {
     selectedFile: null,
     project: null,
     chip: null,
     file: null,
     cmd: null
-
   }
+
+  
 
   cmdSelectedHandler = (event) =>{
     this.state.cmd = event.target.value; 
@@ -69,45 +72,62 @@ class App extends Component {
   }
 
   fileUploadHandler = () => {
-    if(this.state.selectedFile !== null)
-    {
+    
+    if(this.state.cmd == addAllCMD){ // 
+    }
+    
+    if(this.state.cmd == addProjectCMD){ // 
+    }
 
-      console.log(this.state.selectedFile); //stat object has other properties ex: this.state.selectedFile.name
-      var file = this.state.selectedFile;
-      
-      var fileSize = this.state.selectedFile.size; 
-      var fileNameSize = this.state.selectedFile.name.length; 
-      var projectNameSize = this.state.project.length;
-      var chipNameSize = this.state.chip.length;
+    if(this.state.cmd == addChipCMD){ // 
+    }
 
-      var fileSizeDigits = Math.floor(Math.log10(fileSize)) + 1; //decimal digits in file size value
-      var fileLengthDigits = Math.floor(Math.log10(fileNameSize)) + 1;
-      var projectLengthDigits = Math.floor(Math.log10(projectNameSize)) + 1;
-      var chipLengthDigits = Math.floor(Math.log10(chipNameSize)) + 1;
+    if(this.state.cmd == delProjectCMD){ // 
+    }
 
-      var fileAdd = `${startByte}${addFileCMD}${fileLengthDigits}${this.state.selectedFile.name.length}${this.state.selectedFile.name}`;
-      var fileData = `${fileSizeDigits}${fileSize}`;
-      var project = `${projectLengthDigits}${this.state.project.length}${this.state.project}`;
-      var chip = `${chipLengthDigits}${this.state.chip.length}${this.state.chip}`;
-      
+    if(this.state.cmd == delChipCMD){ // 
+    }
 
-      var reader = new FileReader();
-      reader.onload = function () {
-        puck.write(reader.result, callbackTx);
-        puck.write(fileAdd.concat(chip, project, fileData), callbackTx); // concatenate project, chip, and file
+    if(this.state.cmd == delfileCMD){ // 
+    }
+
+    if(this.state.cmd == addFileCMD){ // 
+      if(this.state.selectedFile !== null){
+
+        console.log(this.state.selectedFile); //stat object has other properties ex: this.state.selectedFile.name
+        var file = this.state.selectedFile;
+        
+        var fileSize = this.state.selectedFile.size; 
+        var fileNameSize = this.state.selectedFile.name.length; 
+        var projectNameSize = this.state.project.length;
+        var chipNameSize = this.state.chip.length;
+
+        var fileSizeDigits = Math.floor(Math.log10(fileSize)) + 1; //decimal digits in file size value
+        var fileLengthDigits = Math.floor(Math.log10(fileNameSize)) + 1;
+        var projectLengthDigits = Math.floor(Math.log10(projectNameSize)) + 1;
+        var chipLengthDigits = Math.floor(Math.log10(chipNameSize)) + 1;
+
+        var fileAdd = `${startByte}${addFileCMD}${fileLengthDigits}${this.state.selectedFile.name.length}${this.state.selectedFile.name}`;
+        var fileData = `${fileSizeDigits}${fileSize}`;
+        var project = `${projectLengthDigits}${this.state.project.length}${this.state.project}`;
+        var chip = `${chipLengthDigits}${this.state.chip.length}${this.state.chip}`;
+        
+              //function needs to take selected command and append necessary data 
+        var reader = new FileReader();
+        reader.onload = function () {
+          puck.write(reader.result, callbackTx);
+          puck.write(fileAdd.concat(chip, project, fileData), callbackTx); // concatenate project, chip, and file
+        }
+        reader.readAsBinaryString(file); //now available in the result attribute
       }
-      reader.readAsBinaryString(file); //now available in the result attribute
     }
   }
 
   render(){
     return (
       <div className="App">
-
         <header className="App-header">
-
           <h2>FlashBlaster</h2>
-        
           <p>
             <Button variant="outlined" color="secondary" onClick={this.fileUploadHandler}> 
               Update Device 
@@ -123,11 +143,11 @@ class App extends Component {
             <InputLabel>Select Command</InputLabel>
               <Select
                 onChange={this.cmdSelectedHandler}
-                >
+                ref={this.selectRef}
+              >
                 <MenuItem value="">
-                <em>None</em>
+                  <em>None</em>
                 </MenuItem>
-              
                 <MenuItem value={"30"}>Add File</MenuItem>
                 <MenuItem value={"20"}>Add Chip</MenuItem>
                 <MenuItem value={"10"}>Add Project</MenuItem>
@@ -135,17 +155,15 @@ class App extends Component {
                 <MenuItem value={"60"}>Delete File</MenuItem>
                 <MenuItem value={"50"}>Delete Chip</MenuItem>
                 <MenuItem value={"40"}>Delete Project</MenuItem>
-
               </Select>
             {/* </FormControl> */}
             <br />
             <br />
             
-
             Project: 
             <Input type="text" onChange={this.projectNameHandler}/>
             <br />
-            
+          
             Chip: 
             <Input type="text" onChange={this.chipNameHandler}/>
             <br />
@@ -166,9 +184,7 @@ class App extends Component {
 
           </Box>
         </header>
-
       </div>
-
     );
   }
 }
